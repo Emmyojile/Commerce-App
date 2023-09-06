bdb511a5cb76a36a89292211ef7ac9b1efc71058
Emmyojile <emmaojile99@gmail.com>
2023-09-06 15:31:19 -0800

    network issue

--- server/app.js
+++ server/app.js
@@ -6,7 +6,7 @@ const PORT = process.env.PORT || 5000;
 const cors = require("cors")
 
 const corsOptions = {
-    origin: ["*", "http://localhost:5174"], // Update this with the allowed origins or set it to a specific origin
+    origin: ["*", "http://localhost:5173"], // Update this with the allowed origins or set it to a specific origin
     methods: "GET, POST, PUT, DELETE", // Update with the allowed HTTP methods
     allowedHeaders: "Content-Type, Authorization", // Update with the allowed headers
     credentials: true, // Enable credentials if you're using cookies or other authentication methods
