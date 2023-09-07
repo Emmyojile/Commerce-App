512bad0d1877a58b58315ac69e8d36d74d554b28
Emmyojile <emmaojile99@gmail.com>
2023-09-06 20:30:44 -0800

    fetching data issue

--- client/src/components/Products.jsx
+++ client/src/components/Products.jsx
@@ -5,32 +5,45 @@ import { useEffect, useState } from "react";
 import axios from "axios";
 
 const Container = styled.div`
-    padding: 20px;
-    display: flex;
-    flex-wrap: wrap;
-    justify-content: space-between;
+  padding: 20px;
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: space-between;
 `;
 
-const Products = ({cat, filters, sort}) => {
-
+const Products = ({ cat, filters, sort }) => {
   const [products, setProducts] = useState([]);
   const [filteredProducts, setFilteredProducts] = useState([]);
 
   useEffect(() => {
-      const getProducts = async () => {
-        try {
-          const res = await axios.get('/products')
-          console.log(res);
-        } catch (err) {
-          
-        }
-      }
-      getProducts();
-  }, [cat])
+    const getProducts = async () => {
+      try {
+        const res = await axios.get(
+           `/products`
+        );
+        console.log(res.data);
+        setProducts(res.data);
+      } catch (err) {}
+    };
+    getProducts();
+  }, [cat]);
+
+  // useEffect(() => {
+  //   cat &&
+  //     setFilteredProducts(
+  //       products.filter((item) =>
+  //         Object.entries(filters).every(([key, value]) =>
+  //           item[key].includes(value)
+  //         )
+  //       )
+  //     );
+  // }, [products, cat, filters]);
+
+  // console.log(filteredProducts);
 
   return (
     <Container>
-      {popularProducts.map((item) => (
+      {filteredProducts.map((item) => (
         <Product item={item} key={item.id} />
       ))}
     </Container>
